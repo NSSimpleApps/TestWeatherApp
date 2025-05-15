44f8bd145791f28d239802fb44c9b955c73084ab
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2025-05-15 22:46:06 +0700

    Обновление.

--- TestWeatherApp/SceneDelegate.swift
+++ TestWeatherApp/SceneDelegate.swift
@@ -14,25 +14,25 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = scene as? UIWindowScene else { return }
         
 //        Проверка данных от api.weatherapi.com.
-//        let weatherApiRequestBuilder = WeatherApiRequestBuilder(key: WEATHER_API_KEY)
-//        let weatherAppNetworkProvider = WeatherAppNetworkProvider(requestBuilder: weatherApiRequestBuilder)
-//        let weatherApiHandler = WeatherApiHandler(weatherApiProvider: weatherAppNetworkProvider)
+        let weatherApiRequestBuilder = WeatherApiRequestBuilder(key: WEATHER_API_KEY)
+        let weatherAppNetworkProvider = WeatherAppNetworkProvider(requestBuilder: weatherApiRequestBuilder)
+        let weatherApiHandler = WeatherApiHandler(weatherApiProvider: weatherAppNetworkProvider)
         
 //        Для проверки локальных данных от api.weatherapi.com.
 //        let weatherApiTestProvider = WeatherApiTestProvider()
 //        let weatherApiHandler = WeatherApiHandler(weatherApiProvider: weatherApiTestProvider)
         
         // Проверка данных от api.openweathermap.com.
-        let openWeatherMapRequestBuilder = OpenWeatherMapRequestBuilder(appid: OPEN_WEATHER_APPID)
-        let weatherAppNetworkProvider = WeatherAppNetworkProvider(requestBuilder: openWeatherMapRequestBuilder)
-        let openWeatherMapApiHandler = OpenWeatherMapApiHandler(weatherApiProvider: weatherAppNetworkProvider,
-                                                                screenScale: Int(windowScene.screen.scale))
+//        let openWeatherMapRequestBuilder = OpenWeatherMapRequestBuilder(appid: OPEN_WEATHER_APPID)
+//        let weatherAppNetworkProvider = WeatherAppNetworkProvider(requestBuilder: openWeatherMapRequestBuilder)
+//        let openWeatherMapApiHandler = OpenWeatherMapApiHandler(weatherApiProvider: weatherAppNetworkProvider,
+//                                                                screenScale: Int(windowScene.screen.scale))
         
 //        Для проверки локальных данных от api.openweathermap.com.
 //        let openWeatherMapTestProvider = OpenWeatherMapTestProvider()
 //        let openWeatherMapApiHandler = OpenWeatherMapApiHandler(weatherApiProvider: openWeatherMapTestProvider)
         
-        let weatherProvider: any WeatherAppProviderProtocol = openWeatherMapApiHandler
+        let weatherProvider: any WeatherAppProviderProtocol = weatherApiHandler
         
         let window = UIWindow(windowScene: windowScene)
         window.overrideUserInterfaceStyle = .light
